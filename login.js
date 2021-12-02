7dd81f957fe8e410e4ce0bf36f63e9d021274ae3
suneelyarramuthala <70442430+suneelyarramuthala@users.noreply.github.com>
2021-12-02 16:12:25 +0530

    Update login.js

--- login.js
+++ login.js
@@ -3,15 +3,14 @@ var attempt = 3; // Variable to count number of attempts.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
+if ( username == "suneel" && password == "suneel#123"){
 alert ("Login successfully");
 window.location = "success.html"; // Redirecting to other page.
 return false;
 }
 else{
-attempt --;// Decrementing by one.
+attempt --;
 alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
